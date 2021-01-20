ae2b7d7db0563b05a755c9e380bc6e01c2e3e30e
lưu văn thạnh <thanhlv@d-soft.com.vn>
2021-01-20 18:03:53 +0700

    update

--- src/distance.py
+++ src/distance.py
@@ -4,43 +4,23 @@ import numpy as np
 import cv2
 
 
-class Distance():
-    def __init__(self, landmark=None, img_path=None):
+class FacePose():
+    def __init__(self, landmark, img_path=None):
         """This module used to align face image and evaluate face pose of face   image
 
         Args:
             landmark ([type], optional): Five point of face landmark. Defaults to None.
         """
-        self.landmark = landmark
+        self.landmark = landmark['keypoints']
+        self.box = landmark['box']
         self.img = cv2.imread(img_path)
-        self.pose_angle = None
+        self.head_tilt = None
+        self.nose_deviation = None
 
 
-    def check_face_pose(self):
-        """[summary]
-
-        Returns:
-            [float]: ratio distance between left eye and right eye
-        """
-        mid_mouth = (round((self.landmark['mouth_left'][0] + self.landmark['mouth_right'][0])/2), \
-                        round((self.landmark['mouth_right'][1] + self.landmark['mouth_left'][1])/2))
-        
-        d_left_eye = abs(self._measure_distance(self.landmark['nose'], mid_mouth,
-                                                self.landmark['left_eye']))
-        d_right_eye = abs(self._measure_distance(self.landmark['nose'], mid_mouth,
-                                                self.landmark['right_eye']))
-
-        # Check distance
-        if d_left_eye > d_right_eye:
-            ratio = d_right_eye / d_left_eye
-        elif d_left_eye < d_right_eye:
-            ratio = d_left_eye / d_right_eye
-        else:
-            ratio = 1
-        return ratio
 
 
-    def _measure_distance(self, point_a, point_b, point_c):
+    def _calc_distance(self, point_a, point_b, point_c):
         """ This function to calculate distance from a point to the line which created by two points
 
         Args:
@@ -57,29 +37,54 @@ class Distance():
         return np.cross(pb-pa, pc-pa) / np.linalg.norm(pb-pa)
 
 
-    def measure_angle_eyes(self):
+    def calc_face_tilt(self):
         """ Measure angle according to eyes cordinate"""
 
         # Draw line between left eye and right eye
+        # TODO: Remove draw
         img = cv2.line(self.img, self.landmark['left_eye'],\
                     self.landmark['right_eye'], color=(255, 0, 0), thickness=2)
 
         img = cv2.circle(img, self.landmark['left_eye'],\
-                            radius=0, color=(0,0,255), thickness=2)
+                            radius=0, color=(0, 0, 255), thickness=2)
         img = cv2.circle(img, self.landmark['nose'], \
                             radius=0, color=(0, 0, 255), thickness=10)
+        img = cv2.circle(img, self.landmark['mouth_left'], \
+                            radius=0, color=(0, 0, 255), thickness=10)
+        img = cv2.circle(img, self.landmark['mouth_right'], \
+                            radius=0, color=(0, 0, 255), thickness=10)
+        img = cv2.rectangle(img, (self.box[0], self.box[1]), (self.box[0] + self.box[2], self.box[1] + self.box[3]), color=(255, 0, 0), thickness=4)
 
+
+        cv2.imshow('image', img)
+        cv2.waitKey(0)
 
         if self.landmark['left_eye'][1] < self.landmark['right_eye'][1]:
             y = self.landmark['right_eye'][1] - self.landmark['left_eye'][1]
             x = self.landmark['right_eye'][0] - self.landmark['left_eye'][0]
-            self.pose_angle = np.arctan2(y, x)
-            print(self.pose_angle)
+            self.tilt_angle = np.arctan2(y, x)
+            print(self.tilt_angle)
 
         else:
             y = self.landmark['left_eye'][1] - self.landmark['right_eye'][1]
             x = self.landmark['right_eye'][0] - self.landmark['left_eye'][0]
-            self.pose_angle = np.arctan2(y, x)
-            print(self.pose_angle)
+            self.tilt_angle = np.arctan2(y, x)
+            print(self.tilt_angle)
 
-        return self.pose_angle
+        return self.tilt_angle
+
+
+    def calc_nose_deviation(self):
+        """ Calculate nose deviation according to distance ratio
+
+        Returns:
+            [float]: Ratio
+        """
+        # calc x box center
+        x_center = self.box[0] + self.box[3]/2
+        nose_center_dis = abs(self.landmark['nose'][0] - x_center)
+        self.nose_deviation = nose_center_dis / (self.box[2]/2) * 0.8
+        print('nose_distance', nose_center_dis)
+        print('center', self.box[2]/2)
+        print('nose distance ', self.nose_deviation)
+        return self.nose_deviation
